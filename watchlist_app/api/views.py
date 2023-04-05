d603c5c911de7b6d41b71541e16e279b14476f69
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-04-05 17:54:15 +0100

    status_code

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from . serializers import MovieSerializer
@@ -17,13 +18,18 @@ def movie_list(request):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
 
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({
+                "error": "Movie not found"
+            }, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -34,9 +40,9 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
