6de1df633929a6cb0b0442ec45f47596c5752d8a
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-04-06 15:42:12 +0100

    API CLASS-BASEVIEW COMPLETED

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from .views import movie_list, movie_details
+# from .views import movie_list, movie_details
+from .views import MovieDetailAV, MovieListAV
 
 urlpatterns = [
-    path('list', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-detail')
+    # path('list', movie_list, name='movie-list'),
+    # path('<int:pk>', movie_details, name='movie-detail')
+    path('list', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie-detail')
+    
 ]
