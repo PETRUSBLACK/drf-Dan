42ccb639111cae15618c9a0fc74face9a69ff47a
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-04-20 16:05:50 +0100

    'viewset

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,9 +1,26 @@
+from django.shortcuts import get_object_or_404 
 from rest_framework.response import Response
 from rest_framework import status, mixins, generics
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import viewsets 
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from . serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
+
+
+# This is the viewset view
+class StreamPlatformVS(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        watchlist = get_object_or_404(queryset, pk=pk)
+        serializer = StreamPlatformSerializer(watchlist)
+        return Response(serializer.data)
 
 
 class ReviewList(generics.ListAPIView):
