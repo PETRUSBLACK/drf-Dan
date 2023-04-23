5b8df1b79575c21d1416cdf670805f4bcba83f60
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-04-23 18:07:59 +0100

    viewset completed

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -22,6 +22,14 @@ class StreamPlatformVS(viewsets.ViewSet):
         serializer = StreamPlatformSerializer(watchlist)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 class ReviewList(generics.ListAPIView):
     queryset = Review.objects.all()
